e12f579995fd03b090c0cd63d0ff9fdaed0d6edc
ayakovis <ayakovis@objectsystems.com>
2026-04-21 02:30:16 -0400

    initial commit: set up MyWebAppFastEndpoints project with FastEndpoints integration and basic configuration

---
+++ MyRequest.cs
@@ -0,0 +1,6 @@
+public class MyRequest
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public int Age { get; set; }
+}
---
+++ MyResponse.cs
@@ -0,0 +1,5 @@
+public class MyResponse
+{
+    public required string FullName { get; set; }
+    public bool IsOver18 { get; set; }
+}
